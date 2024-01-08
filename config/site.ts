2969c4250be99ebef5e1b985765cf29c76ce8889
Thanush Arulanantham <121514387+thanarul@users.noreply.github.com>
2024-01-08 15:25:03 -0500

    Add files via upload

---
+++ config/site.ts
@@ -0,0 +1,14 @@
+export type SiteConfig = typeof siteConfig
+
+export const siteConfig = {
+  name: "Flair",
+  description: "Expertly designed goods for thr workspace goods and travel",
+  footer: [
+    { name: "Home", href: "/" },
+    { name: "About", href: "#" },
+    { name: "Terms & Conditions", href: "#" },
+    { name: "Shipping & Return Policy", href: "#" },
+    { name: "Privacy Policy", href: "#" },
+    { name: "FAQ", href: "#" },
+  ],
+}
